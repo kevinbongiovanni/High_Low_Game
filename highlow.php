817dc0fc1264ad51e5f944277712bb77164f1e1f
Kevin Bongiovanni <whoppingbark@gmail.com>
2014-11-24 14:21:04 -0600

    high low game

--- highlow.php
+++ highlow.php
@@ -1,24 +1,61 @@
 <?php
 
+// // Dump arg count
+// var_dump($argc);
+// // Dump arg vars
+// var_dump($argv);
+// // Exit with 0 errors
 
-$random_number = mt_rand(1, 100);
+// Check for an argument count of 3
+if ($argc == 3) {
 
-echo " Guess a random number."; 
+	// $high = ($argv[1] > $argv[2]) ? $argv[1] : $argv[2];
+	// $low = ($argv[1] < $argv[2]) ? $argv[1] : $argv[2];
+
+	$low = $argv[1];
+	$high = $argv[2];
+
+	$low = (is_numeric($argv[1])) ? $argv[1] : 1;
+
+	if(is_numeric($argv[1])) {
+
+		$low = $argv[1];
+		
+	} else {
+		$low = 1;
+	}
+
+} else {
+	$low = 1;
+	$high = 100;
+}
+
+$random = mt_rand($low, $high);
+
+echo "Guess from $low to $high." . PHP_EOL;
+
+
+	# code...
+
+	// if that argument count is three, 
+	// then use argument 2 to set the min
+	// use argument 3 to set the max
+
+	// mt_rand($min, $max);
+
+echo "Pick a random number."; 
 
 $number_guess = fgets(STDIN);
 
-do {
 
-	// if ($number_guess != int) {
-	// 	echo "That is not a number!" . PHP_EOL;
+do {
 	
-	
-	if ($number_guess > $random_number){
+	if ($number_guess > $random){
 		fwrite(STDOUT, "Guess lower.\n");
 
 	} 
 
-	elseif ($number_guess < $random_number) {
+	elseif ($number_guess < $random) {
 		echo "Guess higher.\n";
 	} 
 
